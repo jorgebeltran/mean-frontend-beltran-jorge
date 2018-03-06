871a6d0c8ceeea36c1b4b30eed57111f592a2ec1
jorgebeltran <yo_coqui@hotmail.com>
2018-03-06 19:33:35 -0400

    inicializando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormEditComponent } from './user-form-edit/user-form-edit.component';
+import { UserFormCreateComponent } from './user-form-create/user-form-create.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserListComponent,
+    UserFormEditComponent,
+    UserFormCreateComponent
   ],
   imports: [
     BrowserModule,
