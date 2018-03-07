8c4a153d759d5c761dc3042ed134eadeffe0ffff
jorgebeltran <yo_coqui@hotmail.com>
2018-03-07 19:55:01 -0400

    implementando un metodo getuserlist en servicio usando una lista

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import { User } from '../model/user'
+import { User } from '../model/user';
+
 
 
 @Injectable()
@@ -11,5 +12,25 @@ export class UserService {
   constructor(httpclient: HttpClient) { 
     this.myHttpClient = httpclient;
   }
+  getUserlist():User[]{
+   let userList:User[]=[];
+    userList.push({
+      name: 'Carlos',
+      lastname: 'beltran',
+      username: 'carlos123',
+      email:'carlos@gmail.com',
+      avatar: 'avatarcarolos'
+    });
+    userList.push({
+      name: 'jorge',
+      lastname: 'beltran',
+      username: 'carlos12',
+      email:'cars@gmail.com',
+      avatar: 'avatarcarol'
+    });
 
+      return userList;
+    
+
+  }
 }
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../model/user';
+import {UserService} from '../services/user.service'
 
 
 @Component({
@@ -16,45 +17,9 @@ export class UserListComponent implements OnInit {
     avatar: 'avatarcarolos'
   };*/
   userList:User[]=[];
-  constructor() {
-    this.userList.push({
-            name: 'Carlos 0',
-            lastname: 'Beltran',
-            username: 'carlos123',
-           email: 'carlos@correo.com',
-            avatar: 'avatarCarlos'
-          });
-          this.userList.push({
-            name: 'Carlos 1',
-            lastname: 'Beltran',
-            username: 'carlos123',
-            email: 'carlos@correo.com',
-            avatar: 'avatarCarlos'
-          });
-          this.userList.push({
-            name: 'Carlos 2',
-           lastname: 'Beltran',
-            username: 'carlos123',
-            email: 'carlos@correo.com',
-            avatar: 'avatarCarlos'
-          });
-          this.userList.push({
-            name: 'Carlos 3',
-            lastname: 'Beltran',
-            username: 'carlos123',
-            email: 'carlos@correo.com',
-            avatar: 'avatarCarlos'
-        });
-        this.userList.push({
-          name: 'Carlos 4',
-          lastname: 'Beltran',
-          username: 'carlos123',
-          email: 'carlos@correo.com',
-          avatar: 'avatarCarlos'
-      
-   });
-  }
+  constructor(private userservice:UserService) {  }
   ngOnInit() {
+    this.userList=this.userservice.getUserlist();
   }
 
 }
