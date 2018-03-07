2f76cc105b7ba8d424a21cd2ca35d661d83b7c11
jorgebeltran <yo_coqui@hotmail.com>
2018-03-06 20:45:23 -0400

    data binding interpolacion de un item 5

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -6,5 +6,5 @@ export interface User{
     email:string,
     password?:string,
     avatar:string,
-    type:string
+    type?:string
 }
--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user';
+
 
 @Component({
   selector: 'umss-user-list',
@@ -6,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
-
+  usertest: User= {
+    name: 'Carlos',
+    lastname: 'beltran',
+    username: 'carlos123',
+    email:'carlos@gmail.com',
+    avatar: 'avatarcarolos'
+  };
   constructor() { }
 
   ngOnInit() {
